e59ea14e65568fe3c1d81447fb47acbf1b739eb4
more rushi <morerushikesh1111@gmail.com>
2025-01-03 12:17:21 +0530

    Test

--- Test_Setup.py
+++ Test_Setup.py
@@ -1,3 +1,4 @@
+import requests, time
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from server.loginmgmt.session_manger import SessionManager
@@ -9,32 +10,32 @@ SessionManager.load_credentials()
 chatId = SessionManager.User_Config['chatId']
 bot_id = SessionManager.User_Config['emergency_bot']
 
-
-# Initialize the Chrome WebDriver
-driver = webdriver.Chrome()
-
-
-
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
+
+
+
 # For Linux
 driver = webdriver.Chrome(options=options)
 
-# # For Windows
+# # # For Windows
+# driver = webdriver.Chrome()
+
+
+
 driver.get("https://www.google.com")
 
 
+time.sleep(3)
 
-
-
+print(driver.title)
 # # Close the browser
 
 
 
-import requests
 
 
 
